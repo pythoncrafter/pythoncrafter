2d4cb23a96239b32c4cb5e44108a50d075bc6109
Clifford Butler <149020840+pythoncrafter@users.noreply.github.com>
2023-10-27 06:58:50 -0700

    Update main.py
    added the os module.

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+import os
 
 class ResearchDatabase:
     def __init__(self, db_name):
@@ -157,7 +158,6 @@ class ResearchApp(App):
 
 
 if __name__ == '__main__':
-    db_name = research_db
     research_db = ResearchDatabase('data/research_data.db')
     user = User('data/research_data.db')
     admin = Admin('data/research_data.db')
