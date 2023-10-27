d1bf0fa386810667661c082f4bfb00794f0e8438
Clifford Butler <149020840+pythoncrafter@users.noreply.github.com>
2023-10-27 08:52:05 -0700

    Update edit_view_entries.py
    a script for editing the entries in the database.

--- app/edit_view_entries.py
+++ app/edit_view_entries.py
@@ -1,1 +1,15 @@
+import sqlite3
 
+conn = sqlite3.connect('research_data.db')
+c = conn.cursor()
+
+# Example of editing data in the Players table
+c.execute("UPDATE Players SET PlayerName = ? WHERE PlayerID = ?", ('Jane Doe', 1))
+
+# Example of viewing an entry in the Players table
+c.execute("SELECT * FROM Players WHERE PlayerID = ?", (1,))
+row = c.fetchone()
+print(row)
+
+conn.commit()
+conn.close()
