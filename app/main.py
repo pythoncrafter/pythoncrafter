5b7880c428a0a8229c721a18ece2a2c4269a76f3
Clifford Butler <149020840+pythoncrafter@users.noreply.github.com>
2023-10-27 09:29:24 -0700

    Update main.py
    editing names to work

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@
 
 from create_database import create_tables
 from read_database import read_data
-from write_database import write_data
-from edit_view_entries import edit_entries
+from write_database import generate_random_player_data
+from edit_view_entries import edit_view_entries
 
 def main():
     create_tables()
