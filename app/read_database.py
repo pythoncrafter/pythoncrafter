bb9bf20689057f13842f9876da4a8160b8628f4d
Clifford Butler <149020840+pythoncrafter@users.noreply.github.com>
2023-10-27 08:50:47 -0700

    Update read_database.py
    a way to read and display the information in the database

--- app/read_database.py
+++ app/read_database.py
@@ -1,1 +1,12 @@
+import sqlite3
 
+conn = sqlite3.connect('research_data.db')
+c = conn.cursor()
+
+# Example of reading and displaying data from the Players table
+c.execute("SELECT * FROM Players")
+rows = c.fetchall()
+for row in rows:
+    print(row)
+
+conn.close()
