ee15eb0edef713b4e109ea89ad7b8b18633b6a93
Clifford Butler <149020840+pythoncrafter@users.noreply.github.com>
2023-10-27 09:36:07 -0700

    Update write_database.py
    a real country will be added for the randomization portion.

--- app/write_database.py
+++ app/write_database.py
@@ -1,12 +1,14 @@
 import sqlite3
 import random
+import pycountry
 
 # Function to generate random player data
 def generate_random_player_data():
+    countries = list(pycountry.countries)
     player_data = {
         "PlayerName": f"Player{random.randint(1, 100)}",
         "Age": random.randint(18, 40),
-        "Nationality": f"Country{random.randint(1, 10)}",
+        "Nationality": random.choice(countries).name,
         "Sport": random.choice(["Soccer", "Basketball", "Tennis", "Golf", "Cricket"]),
         "TeamID": random.randint(1, 20),
         "TeamName": f"Team{random.randint(1, 10)}"
