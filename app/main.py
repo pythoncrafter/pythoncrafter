758af8cf7724468d52d3787c90ae63af759e7bdb
Clifford Butler <149020840+pythoncrafter@users.noreply.github.com>
2023-10-27 07:08:21 -0700

    Update main.py
    making sure the files match up. no changes.

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-import os
+import os 
 
 class ResearchDatabase:
     def __init__(self, db_name):
