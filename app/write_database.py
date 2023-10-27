7e27cab3da4a150813d3b28bf46d53a07b60c594
Clifford Butler <149020840+pythoncrafter@users.noreply.github.com>
2023-10-27 08:51:14 -0700

    Update write_database.py
    a script for writing data to the database.

--- app/write_database.py
+++ app/write_database.py
@@ -1,1 +1,11 @@
+import sqlite3
 
+conn = sqlite3.connect('research_data.db')
+c = conn.cursor()
+
+# Example of writing data to the Players table
+c.execute("INSERT INTO Players (PlayerName, Age, Nationality, Sport, TeamID, TeamName) VALUES (?, ?, ?, ?, ?, ?)", 
+          ('John Doe', 25, 'USA', 'Basketball', 1, 'Team A'))
+
+conn.commit()
+conn.close()
