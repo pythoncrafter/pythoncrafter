0f6b4a8779c06c8ce73a93d19577d06c6668fda2
Clifford Butler <149020840+pythoncrafter@users.noreply.github.com>
2023-10-27 08:50:04 -0700

    Update create_database.py
    code for creating database and tables for players, admin questions, coaches and trainers.

--- app/create_database.py
+++ app/create_database.py
@@ -1,1 +1,59 @@
+import sqlite3
+
+conn = sqlite3.connect('research_data.db')
+c = conn.cursor()
+
+# Create the necessary tables
+c.execute('''CREATE TABLE IF NOT EXISTS Players (
+                     PlayerID INTEGER PRIMARY KEY,
+                     PlayerName TEXT,
+                     Age INTEGER,
+                     Nationality TEXT,
+                     Sport TEXT,
+                     TeamID INTEGER,
+                     TeamName TEXT
+                     )''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS TrainingActivities (
+                     ActivityID INTEGER PRIMARY KEY,
+                     Sport TEXT,
+                     Description TEXT
+                     )''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS DecisionMaking (
+                     DecisionID INTEGER PRIMARY KEY,
+                     PlayerID INTEGER,
+                     ActivityID INTEGER,
+                     Sport TEXT,
+                     Question TEXT,
+                     Response TEXT,
+                     FOREIGN KEY (PlayerID) REFERENCES Players(PlayerID),
+                     FOREIGN KEY (ActivityID) REFERENCES TrainingActivities(ActivityID)
+                     )''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS PlayerTraining (
+                     PlayerID INTEGER,
+                     ActivityID INTEGER,
+                     FOREIGN KEY (PlayerID) REFERENCES Players(PlayerID),
+                     FOREIGN KEY (ActivityID) REFERENCES TrainingActivities(ActivityID)
+                     )''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS Coaches (
+                     CoachID INTEGER PRIMARY KEY,
+                     CoachName TEXT,
+                     Experience TEXT,
+                     TeamID INTEGER,
+                     TeamName TEXT
+                     )''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS Trainers (
+                     TrainerID INTEGER PRIMARY KEY,
+                     TrainerName TEXT,
+                     Specialization TEXT,
+                     TeamID INTEGER,
+                     TeamName TEXT
+                     )''')
+
+conn.commit()
+conn.close()
 
