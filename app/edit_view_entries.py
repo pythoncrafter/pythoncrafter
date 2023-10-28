5de4640ca130d76fbe0cc061d2d80b71b4f3f009
Clifford Butler <149020840+pythoncrafter@users.noreply.github.com>
2023-10-27 18:40:58 -0700

    Update edit_view_entries.py

--- app/edit_view_entries.py
+++ app/edit_view_entries.py
@@ -1,14 +1,14 @@
 import sqlite3
 
-# Function to view and edit entries
-def view_edit_entries():
+# Function to view, edit, and delete entries
+def view_edit_delete_entries():
     conn = sqlite3.connect('research_data.db')
     c = conn.cursor()
 
     print("1. View all players")
     print("2. View all coaches")
     print("3. Edit a player's age")
-
+    print("4. Delete a player")
     choice = input("Enter your choice: ")
 
     if choice == "1":
@@ -28,6 +28,10 @@ def view_edit_entries():
         new_age = input("Enter the new age for the player: ")
         c.execute("UPDATE Players SET Age = ? WHERE PlayerID = ?", (new_age, player_id))
         print("\nPlayer's age updated.")
+    elif choice == "4":
+        player_id = input("Enter the PlayerID of the player you want to delete: ")
+        c.execute("DELETE FROM Players WHERE PlayerID = ?", (player_id,))
+        print("\nPlayer deleted.")
     else:
         print("Invalid choice.")
 
@@ -35,5 +39,5 @@ def view_edit_entries():
     conn.close()
 
 if __name__ == '__main__':
-    # Call the function to view and edit entries
-    view_edit_entries()
+    # Call the function to view, edit, and delete entries
+    view_edit_delete_entries()
