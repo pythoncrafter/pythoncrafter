22cfeb0c2dc101646e02dc2260125abf78739539
Clifford Butler <149020840+pythoncrafter@users.noreply.github.com>
2023-10-27 18:30:17 -0700

    Update main.py
    modifying what code has commented out

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@
 #from create_database import create_tables
 #from write_database import generate_random_player_data
 #from write_database import print_generated_data
-from read_database import read_data
-#from edit_view_entries import edit_view_entries
+#from read_database import read_data
+from edit_view_entries import edit_view_entries
 
 def main():
     #create_tables()
@@ -12,11 +12,10 @@ def main():
     #generate_random_player_data()
     #print_generated_data()
    
-    read_data()
-    print("Data read from the database.")
+    #read_data()
 
-    #edit_view_entries()
-    #print("Entries edited and viewed from the database.")
+    edit_view_entries()
+    print("Entries edited and viewed from the database.")
 
 
 if __name__ == '__main__':
