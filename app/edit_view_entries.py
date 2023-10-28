24239f0130e8a011eddd98c8aa0234b1edff23f1
Clifford Butler <149020840+pythoncrafter@users.noreply.github.com>
2023-10-27 18:33:50 -0700

    Update edit_view_entries.py
    making the edit file function the way as inteded.

--- app/edit_view_entries.py
+++ app/edit_view_entries.py
@@ -1,34 +1,39 @@
 import sqlite3
 
-# Function to edit and view entries
-def edit_view_entries():
+# Function to view and edit entries
+def view_edit_entries():
     conn = sqlite3.connect('research_data.db')
     c = conn.cursor()
 
-    print("Viewing all players:")
-    c.execute("SELECT * FROM Players")
-    players = c.fetchall()
-    for player in players:
-        print(player)
+    print("1. View all players")
+    print("2. View all coaches")
+    print("3. Edit a player's age")
 
-    print("\nViewing all coaches:")
-    c.execute("SELECT * FROM Coaches")
-    coaches = c.fetchall()
-    for coach in coaches:
-        print(coach)
+    choice = input("Enter your choice: ")
 
-    print("\nEditing a player's age:")
-    c.execute("UPDATE Players SET Age = 25 WHERE PlayerID = 1")
-
-    print("\nViewing all players after edit:")
-    c.execute("SELECT * FROM Players")
-    players = c.fetchall()
-    for player in players:
-        print(player)
+    if choice == "1":
+        print("\nViewing all players:")
+        c.execute("SELECT * FROM Players")
+        players = c.fetchall()
+        for player in players:
+            print(player)
+    elif choice == "2":
+        print("\nViewing all coaches:")
+        c.execute("SELECT * FROM Coaches")
+        coaches = c.fetchall()
+        for coach in coaches:
+            print(coach)
+    elif choice == "3":
+        player_id = input("Enter the PlayerID of the player you want to edit: ")
+        new_age = input("Enter the new age for the player: ")
+        c.execute("UPDATE Players SET Age = ? WHERE PlayerID = ?", (new_age, player_id))
+        print("\nPlayer's age updated.")
+    else:
+        print("Invalid choice.")
 
     conn.commit()
     conn.close()
 
 if __name__ == '__main__':
-    # Call the function to edit and view entries
-    edit_view_entries()
+    # Call the function to view and edit entries
+    view_edit_entries()
